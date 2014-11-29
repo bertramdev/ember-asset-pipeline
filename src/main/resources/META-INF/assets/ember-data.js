fcd67da56cc92c424e2aa3d648234e3b05456985
David Estes <davydotcom@gmail.com>
2014-11-29 08:47:38 -0600

    first commit

---
+++ src/main/resources/META-INF/assets/ember-data.js
@@ -0,0 +1,2 @@
+//= require ember-data/${grails.util.Environment.current == grails.util.Environment.DEVELOPMENT ? 'ember-data.js' : 'ember-data.prod.js'}
+
---
+++ src/main/resources/META-INF/assets/ember.js
@@ -0,0 +1,1 @@
+//= require ember/${grails.util.Environment.current == grails.util.Environment.DEVELOPMENT ? 'ember.js' : 'ember.prod.js'}
